8d8dcfad80a36d3c0036eb1e49959a5900037486
Choi Hyeon Jun <che0223@naver.com>
2023-12-08 10:31:17 +0900

    3rd commit
    1. 로직 변경
    1) csv_hw.py
    1-1) find_richest_and_asset()
    - 전체 csv 데이터 저장 부분 삭제(공간 낭비 최소화): 변수 하나로 max 데이터 검사 후 추출
    
    1-2) find_top3_richest_city()
    - 전체 데이터 list로 관리 -> dict로 city_name 값만 집계해서 효율적 관리(공간 낭비 최소화)
    - 전체 데이터 sorted -> heapq 활용(시간 복잡도 최소화)
    
    2) xml_hw.py
    2-1) find_top3_poorest_city()
    - 전체 데이터 list로 관리 -> dict로 city_name 값만 집계해서 효율적 관리(공간 낭비 최소화)
    - 전체 데이터 sorted -> heapq 활용(시간 복잡도 최소화)
    
    2. 버그 수정
    1) xml.hw.py
    - int() 형변환 추가

--- tests/homework/csv_hw.py
+++ tests/homework/csv_hw.py
@@ -1,38 +1,42 @@
 from typing import Tuple
 import csv
+import heapq
 
 def find_richest_and_asset() -> Tuple:
-    list_csv_data = []
-    header_col = None
+    max_csv_data = [] # 0: name, 1: est_asset_dollar
 
     with open('hw_data/assets.csv', encoding='utf-8') as f:
         csv_reader = csv.reader(f)
         header_col = dict((value, idx) for idx, value in enumerate(next(csv_reader)))
 
         for data in csv_reader:
-            list_csv_data.append(data)
+            if not max_csv_data:
+                max_csv_data = [data[header_col['name']], int(data[header_col['est_asset_dollar']])]
 
-    max_csv_data = max(list_csv_data, key=lambda csv_data: csv_data[header_col['est_asset_dollar']])
-    return (max_csv_data[header_col['name']], max_csv_data[header_col['est_asset_dollar']])
+            elif max_csv_data[1] < int(data[header_col['est_asset_dollar']]): # 0: name, 1: est_asset_dollar
+                max_csv_data = [data[header_col['name']], int(data[header_col['est_asset_dollar']])]
 
+    return tuple(max_csv_data)
 
 def find_top3_richest_city() -> list:
-    list_csv_data = []
-    header_col = None
+    dict_city = {} # key: city, value: [sum(est_asset_dollar), count]
+    list_heapq = [] # 0: -sum(est_asset_dollar)/count, 1: city
 
     with open('hw_data/assets.csv', encoding='utf-8') as f:
         csv_reader = csv.reader(f)
         header_col = dict((value, idx) for idx, value in enumerate(next(csv_reader)))
 
-        for data in csv_reader:
-            list_csv_data.append(data)
+        for csv_data in csv_reader:
+            if csv_data[header_col['city']] in dict_city:
+                dict_city[csv_data[header_col['city']]][0] += int(csv_data[header_col['est_asset_dollar']])  # sum(est_asset_dollar)
+                dict_city[csv_data[header_col['city']]][1] += 1  # count
+            else:
+                dict_city[csv_data[header_col['city']]] = [int(csv_data[header_col['est_asset_dollar']]), 1]
 
-    dict_city = {}
-    for csv_data in list_csv_data:
-        if csv_data[header_col['city']] in dict_city:
-            dict_city[csv_data[header_col['city']]].append(int(csv_data[header_col['est_asset_dollar']]))
-        else:
-            dict_city[csv_data[header_col['city']]] = [int(csv_data[header_col['est_asset_dollar']])]
+    for city_name, city_data in dict_city.items():
+        heapq.heappush(list_heapq, (-(city_data[0] / city_data[1]), city_name)) # citydata(0: sum(est_asset_dollar), 1: count), list_heapq(0: -sum(est_asset_dollar)/count, 1: city)
 
-    return [items[0] for items in sorted(dict_city.items(), key=lambda items: sum(items[1])/len(items[1]), reverse=True)[:3]]
+    return [heapq.heappop(list_heapq)[1], heapq.heappop(list_heapq)[1], heapq.heappop(list_heapq)[1]] # list_heapq(0: -sum(est_asset_dollar)/count, 1: city)
+
+
 
--- tests/homework/xml_hw.py
+++ tests/homework/xml_hw.py
@@ -1,23 +1,30 @@
 from typing import Tuple
 import xmltodict
+import heapq
 
 def find_poor_and_asset() -> Tuple:
     with open('hw_data/assets.xml', encoding='utf-8') as f:
         list_xml_data = xmltodict.parse(f.read())['data']['asset']
 
-    min_est_asset_dollar_data = min(list_xml_data, key=lambda xml_data:xml_data['est_asset_dollar'])
+    min_est_asset_dollar_data = min(list_xml_data, key=lambda xml_data:int(xml_data['est_asset_dollar']))
 
-    return (min_est_asset_dollar_data['name'], min_est_asset_dollar_data['est_asset_dollar'])
+    return (min_est_asset_dollar_data['name'], int(min_est_asset_dollar_data['est_asset_dollar']))
 
 def find_top3_poorest_city() -> list:
+    dict_city = {}  # key: name, value: [sum(est_asset_dollar), count]
+    list_heapq = []  # 0: -sum(est_asset_dollar)/count, 1: city_name
+
     with open('hw_data/assets.xml', encoding='utf-8') as f:
         list_xml_data = xmltodict.parse(f.read())['data']['asset']
 
-    dict_city = {}
     for xml_data in list_xml_data:
         if xml_data['name'] in dict_city:
-            dict_city[xml_data['name']].append(int(xml_data['est_asset_dollar']))
+            dict_city[xml_data['name']][0] += int(xml_data['est_asset_dollar']) # sum(est_asset_dollar)
+            dict_city[xml_data['name']][1] += 1 # count
         else:
-            dict_city[xml_data['name']] = [int(xml_data['est_asset_dollar'])]
+            dict_city[xml_data['name']] = [int(xml_data['est_asset_dollar']), 1]
 
-    return [items[0] for items in sorted(dict_city.items(), key=lambda items: sum(items[1])/len(items[1]))[:3]]
+    for city_name, city_data in dict_city.items():
+        heapq.heappush(list_heapq,((city_data[0] / city_data[1]), city_name))  # citydata(0: sum(est_asset_dollar), 1: count), list_heapq(0: sum(est_asset_dollar)/count, 1: city_name)
+
+    return [heapq.heappop(list_heapq)[1], heapq.heappop(list_heapq)[1], heapq.heappop(list_heapq)[1]]  # list_heapq(0: sum(est_asset_dollar)/count, 1: city_name)
