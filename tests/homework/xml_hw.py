f12ff81d98efc95d09057e8d81722732f78c265a
Choi Hyeon Jun <che0223@naver.com>
2023-12-09 13:40:21 +0900

    5th commit
    4th commit

--- tests/homework/xml_hw.py
+++ tests/homework/xml_hw.py
@@ -12,19 +12,19 @@ def find_poor_and_asset() -> Tuple:
 
 def find_top3_poorest_city() -> list:
     dict_city = {}  # key: name, value: [sum(est_asset_dollar), count]
-    list_heapq = []  # 0: -sum(est_asset_dollar)/count, 1: city_name
+    list_heapq = []  # [0: -sum(est_asset_dollar)/count,1: city_name]
 
     with open('hw_data/assets.xml', encoding='utf-8') as f:
         list_xml_data = xmltodict.parse(f.read())['data']['asset']
 
     for xml_data in list_xml_data:
-        if xml_data['name'] in dict_city:
-            dict_city[xml_data['name']][0] += int(xml_data['est_asset_dollar']) # sum(est_asset_dollar)
-            dict_city[xml_data['name']][1] += 1 # count
+        if xml_data['city'] in dict_city:
+            dict_city[xml_data['city']][0] += int(xml_data['est_asset_dollar']) # sum(est_asset_dollar)
+            dict_city[xml_data['city']][1] += 1 # count
         else:
-            dict_city[xml_data['name']] = [int(xml_data['est_asset_dollar']), 1]
+            dict_city[xml_data['city']] = [int(xml_data['est_asset_dollar']), 1]
 
     for city_name, city_data in dict_city.items():
-        heapq.heappush(list_heapq,((city_data[0] / city_data[1]), city_name))  # citydata(0: sum(est_asset_dollar), 1: count), list_heapq(0: sum(est_asset_dollar)/count, 1: city_name)
+        heapq.heappush(list_heapq,((city_data[0] / city_data[1]), city_name))  # citydata[0: sum(est_asset_dollar), 1: count], list_heapq[0: sum(est_asset_dollar)/count, 1: city_name]
 
-    return [heapq.heappop(list_heapq)[1], heapq.heappop(list_heapq)[1], heapq.heappop(list_heapq)[1]]  # list_heapq(0: sum(est_asset_dollar)/count, 1: city_name)
+    return [heapq.heappop(list_heapq)[1], heapq.heappop(list_heapq)[1], heapq.heappop(list_heapq)[1]]  # list_heapq[0: sum(est_asset_dollar)/count, 1: city_name]
