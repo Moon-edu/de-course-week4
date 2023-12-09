2e77f4014adc6e5bf5de87868b75d4ef5b7d8817
Choi Hyeon Jun <che0223@naver.com>
2023-12-09 12:21:24 +0900

    4th commit
    1. 버그 수정
    1) xml_hw.py
    find_top3_poorest_city()
    'name' -> 'city'로 수정

--- tests/homework/csv_hw.py
+++ tests/homework/csv_hw.py
@@ -3,7 +3,7 @@ import csv
 import heapq
 
 def find_richest_and_asset() -> Tuple:
-    max_csv_data = [] # 0: name, 1: est_asset_dollar
+    max_csv_data = [] # [0: name, 1: est_asset_dollar]
 
     with open('hw_data/assets.csv', encoding='utf-8') as f:
         csv_reader = csv.reader(f)
@@ -13,14 +13,14 @@ def find_richest_and_asset() -> Tuple:
             if not max_csv_data:
                 max_csv_data = [data[header_col['name']], int(data[header_col['est_asset_dollar']])]
 
-            elif max_csv_data[1] < int(data[header_col['est_asset_dollar']]): # 0: name, 1: est_asset_dollar
+            elif max_csv_data[1] < int(data[header_col['est_asset_dollar']]): # max_csv_data[0: name, 1: est_asset_dollar]
                 max_csv_data = [data[header_col['name']], int(data[header_col['est_asset_dollar']])]
 
     return tuple(max_csv_data)
 
 def find_top3_richest_city() -> list:
     dict_city = {} # key: city, value: [sum(est_asset_dollar), count]
-    list_heapq = [] # 0: -sum(est_asset_dollar)/count, 1: city
+    list_heapq = [] # [0: -sum(est_asset_dollar)/count, 1: city]
 
     with open('hw_data/assets.csv', encoding='utf-8') as f:
         csv_reader = csv.reader(f)
@@ -34,9 +34,9 @@ def find_top3_richest_city() -> list:
                 dict_city[csv_data[header_col['city']]] = [int(csv_data[header_col['est_asset_dollar']]), 1]
 
     for city_name, city_data in dict_city.items():
-        heapq.heappush(list_heapq, (-(city_data[0] / city_data[1]), city_name)) # citydata(0: sum(est_asset_dollar), 1: count), list_heapq(0: -sum(est_asset_dollar)/count, 1: city)
+        heapq.heappush(list_heapq, (-(city_data[0] / city_data[1]), city_name)) # citydata[0: sum(est_asset_dollar), 1: count), list_heapq(0: -sum(est_asset_dollar)/count, 1: city]
 
-    return [heapq.heappop(list_heapq)[1], heapq.heappop(list_heapq)[1], heapq.heappop(list_heapq)[1]] # list_heapq(0: -sum(est_asset_dollar)/count, 1: city)
+    return [heapq.heappop(list_heapq)[1], heapq.heappop(list_heapq)[1], heapq.heappop(list_heapq)[1]] # list_heapq[0: -sum(est_asset_dollar)/count, 1: city]
 
 
 
