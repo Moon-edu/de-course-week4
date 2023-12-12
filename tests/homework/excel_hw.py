6c0575c02666e40213a38e1e13aa9c30de6c7f34
박은지 <sanji77@g.skku.edu>
2023-12-12 11:17:52 +0900

    오타 수정
    변수 오타 수정 (sum_essets > sum_assets)

--- tests/homework/excel_hw.py
+++ tests/homework/excel_hw.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+0from typing import Tuple
 # 주의사항 1. 각 파일 간의 함수, 혹은 로직을 공유하지 마세요
 # 주의사항 2. 함수 이름은 변경하지 마세요
 # 주의사항 3. 함수명이 test_로 시작하는 함수를 만들지 마세요
@@ -21,7 +21,7 @@ def find_corp_total_asset() -> int:
         if not flags:
             flags = True
         elif row[1].value.endswith('Corp.'):
-            sum_essets += int(row[4].value)
+            sum_assets += int(row[4].value)
 
     return sum_assets
 
